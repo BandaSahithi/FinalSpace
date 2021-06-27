52f799c9b25c7054f26a6420cd7187e66b9fcc44
BandaSahithi <84679467+BandaSahithi@users.noreply.github.com>
2021-06-27 22:53:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FinalSpace from './components/FinalSpace';
+
+function App() {
+  return (
+    <div id="id1">
+      <FinalSpace />
+    </div>
+  );
+}
+
+export default App;
